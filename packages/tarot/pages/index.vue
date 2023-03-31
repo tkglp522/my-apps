7ff470d61f9532e15361fc10b68c552cfb1cb4c1
日下 <103241741+tkglp522@users.noreply.github.com>
2023-04-01 01:13:04 +0900

    動的な質問を受け付けられるようにした。

--- packages/tarot/pages/index.vue
+++ packages/tarot/pages/index.vue
@@ -2,25 +2,29 @@
 const promptHistory = ref<Array<string | null>>([])
 const userHistory = ref<Array<string | null>>([])
 const message = ref<string>('')
+const pending = ref<boolean>(false)
 
 const sendMessage = async () => {
+    const question = message.value
+    pending.value = true
     userHistory.value.push(message.value)
     message.value = ''
     const res = await useFetch('/api/openai', {
     method: "POST",
     body: {
-    message: message.value
+    question: question
     }
   })
   const data = await res.data.value
   promptHistory.value.push(data)
+  pending.value = false
 }
 
 
 </script>
 <template>
 <div>
-    <div class="mb-8 bg-secondary">
+    <div class="mb-8">
     <div class="chat chat-start">
     <div class="chat-image avatar">
         <div class="w-10 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
@@ -29,30 +33,27 @@ const sendMessage = async () => {
       </div>
       <div class="chat-bubble">占いたい内容は何ですか？</div>
     </div>
-    <div>
-      <div v-for="userMessage in userHistory" class="chat chat-end">
+    <div v-for="userMessage, index in userHistory">
+      <div  class="chat chat-end">
         <div class="chat-bubble">{{ userMessage }}</div>
       </div>
-    <div v-if="promptHistory[0]" class="chat chat-start">
+      <div v-if="promptHistory[index]" class="chat chat-start">
         <div class="chat-image avatar">
             <div class="w-10 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
               <img src="~/assets/img/tarot_uranai_woman.png" />
             </div>
         </div>
         <div class="card card-side bg-base-200 shadow-xl">
-          <figure class="w-36"><img src="~/assets/img/the-high-priestess-tarot-card.webp" alt="Card"/></figure>
+          <figure class="h-full"><img src="~/assets/img/the-high-priestess-tarot-card.webp" alt="Card"/></figure>
           <div class="card-body">
             <h2 class="card-title">New movie is released!</h2>
-            <p>{{promptHistory[0]}}</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Watch</button>
-            </div>
+            <p>{{promptHistory[index]}}</p>
           </div>
         </div>
-    </div>
+      </div>
     </div>
     </div>
     <input v-model="message" type="text" placeholder="Type here" class="input input-bordered w-full mb-8" />
-    <button class="btn btn-primary w-full" @click="sendMessage">占う</button>
+    <button class="btn btn-primary w-full" @click="sendMessage" :disabled="pending">占う</button>
 </div>
 </template>
--- packages/tarot/server/api/openai.ts
+++ packages/tarot/server/api/openai.ts
@@ -13,21 +13,58 @@ const openai = new OpenAIApi(configuration);
     return;
   }
 
-  const message = body.message || '';
+  const question = body.question || '';
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(message),
+      prompt: generatePrompt(question),
+      temperature: 0.4,
+      max_tokens: 1000
     });
     return completion.data.choices[0].text;
   } catch(error) {
   }
 })
 
-const generatePrompt = (message: string) => {
-  return `You are fortunteller.
-Draw one card from tarot decks to answer the following question.
-Question: ${message}
-Answer:`;
+const generatePrompt = (question: string) => {
+  return `あなたは占い師です。
+  以下の22枚の大アルカナのみからなるタロットデッキを使ってください。
+  0：愚者、
+  1：魔術師、
+  2：女教皇、
+  3：女帝、
+  4：皇帝、
+  5：教皇
+  6：恋人、
+  7：戦車、
+  8：力、
+  9：隠者、
+  10：運命の輪
+  11：正義、
+  12：吊るされた男、
+  13：死神、
+  14：節制、
+  15：悪魔
+  16：塔、
+  17：星、
+  18：月、
+  19：太陽、
+  20：審配、
+  21：世界
+
+  カードは全て正位置とします。
+  タロットカードを１枚引いて、その意味を説明してください。
+
+  そして以下の質問に答えてください。
+
+  ${question}
+
+  以下のフォーマットで答えること
+
+  番号:
+  カード:
+  説明:
+  答え:
+`;
 }
