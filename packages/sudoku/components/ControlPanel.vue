ea025256a0a9eba908d1c99e8103c5a1f4cb7b12
日下 <103241741+tkglp522@users.noreply.github.com>
2023-04-11 17:47:49 +0900

    sudoku app completed

---
+++ packages/sudoku/components/ControlPanel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex justify-center space-x-4">
+        <button @click="generatePuzzle"
+            class="px-4 py-2 text-white bg-blue-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300">
+            New Puzzle
+        </button>
+        <button @click="checkSolution"
+            class="px-4 py-2 text-white bg-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-300">
+            Check Solution
+        </button>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup(_, { emit }) {
+        const generatePuzzle = () => {
+            emit('generate-puzzle');
+        };
+
+        const checkSolution = () => {
+            emit('check-solution');
+        };
+
+        return {
+            generatePuzzle,
+            checkSolution,
+        };
+    },
+});
+</script>
---
+++ packages/sudoku/components/SudokuGrid.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="grid grid-cols-9 gap-0.5">
+        <div v-for="(cell, index) in cells" :key="index" class="w-12 h-12 text-center">
+            <input
+          v-model="cell.value"
+          type="number"
+          min="1"
+          max="9"
+          class="w-full h-full border border-gray-200 focus:border-blue-500 focus:ring-1 focus:ring-blue-200 text-center"
+          :readonly="cell.readonly"
+          @input="updateCellValue(index)"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+import Sudoku from 'sudoku-umd';
+import { onMounted } from 'vue';
+
+export default {
+    setup() {
+
+        onMounted(() => {
+            generatePuzzle();
+        });
+
+        const cells = ref([]);
+        const puzzle = ref('');
+
+        const generatePuzzle = () => {
+            puzzle.value = Sudoku.generate('medium');
+            cells.value = puzzle.value.split('').map((cell, index) => ({
+                value: cell !== '.' ? parseInt(cell) : '',
+                readonly: cell !== '.',
+            }));
+        };
+
+        const checkSolution = () => {
+            const solution = Sudoku.solve(puzzle.value);
+            const userSolution = cells.value.map(cell => cell.value).join('');
+
+            if (solution === userSolution) {
+                alert('Congratulations!You have solved the puzzle correctly!');
+} else {
+                alert('Incorrect solution. Please try again.');
+            }
+        };
+
+        const updateCellValue = (index) => {
+            const value = cells.value[index].value;
+
+            if (value < 1 || value > 9) {
+                cells.value[index].value = '';
+            }
+        };
+
+        return {
+             cells,
+            generatePuzzle,
+            checkSolution,
+            updateCellValue,
+        };
+    },
+};
+
+</script>
+
