ac627c85a58154cff9ec564154f952e316a57a64
日下 <103241741+tkglp522@users.noreply.github.com>
2023-04-13 10:20:04 +0900

    resize header

--- packages/sudoku/app.vue
+++ packages/sudoku/app.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container mx-auto py-10">
-        <h1 class="text-4xl font-bold text-center mb-8">Sudoku</h1>
+    <div class="container mx-auto py-6">
+        <h1 class="text-xl font-bold text-center mb-4">Sudoku</h1>
         <SudokuGrid ref="sudokuGrid" />
     </div>
 </template>
